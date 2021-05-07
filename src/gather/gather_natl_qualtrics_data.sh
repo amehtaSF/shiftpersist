c128075a208e1a99c6bef626544aac9b09575e36
Ashish <ashm@stanford.edu>
2021-05-07 11:26:59 -0700

    created template for natl qualtrics analysis

---
+++ src/gather/gather_natl_qualtrics_data.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+data_project_dir="$HOME/files/research/data/collab/greg_bratman/nature_reappraisal"
+project_dir="$HOME/files/research/projects/collab/greg_bratman/nature_reappraisal"
+
+cp "$data_project_dir/data/raw/Winter 2020 Survey EWB - S2B (National)_April 26, 2021_11.30.csv" "$project_dir/data/raw/Winter_2020_Survey_EWB_S2B_National_April_26_2021_11_30.csv"
+cp "$data_project_dir/lib/Winter_2020_Survey_EWB_S2B_National_April_26_2021_11_30_codebook.xlsx" "$project_dir/lib/Winter_2020_Survey_EWB_S2B_National_April_26_2021_11_30_codebook.xlsx"
+
